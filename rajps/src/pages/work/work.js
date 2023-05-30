470ee462ac7863f05c01eef80397d8301a9c0706
ACEXRA <rasheed.786ahamed@gmail.com>
2023-05-30 09:43:44 +0530

    deploy test

--- rajps/src/pages/work/work.js
+++ rajps/src/pages/work/work.js
@@ -1,28 +1,18 @@
 import React from "react";
-import Card from "../../components/ui/card";
 
 function Work() {
   return (
     <div className="work">
       <h1 className="work_heading">MY WORKS</h1>
       <div className="work_card">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-
-        {/* <img src="" alt="img" />
         <img src="" alt="img" />
         <img src="" alt="img" />
         <img src="" alt="img" />
         <img src="" alt="img" />
         <img src="" alt="img" />
         <img src="" alt="img" />
-        <img src="" alt="img" /> */}
+        <img src="" alt="img" />
+        <img src="" alt="img" />
       </div>
     </div>
   );
